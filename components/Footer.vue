707cbde9ef46b9028e5857923b5b73d0477d0765
Fanny Buhlér <fanny.buhler@hyperisland.se>
2021-09-13 13:37:09 +0200

    media queries

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,42 +1,48 @@
 <template>
   <div class="footer">
-    <h3 class="footer__heading">Maila oss</h3>
-    <form class="footer__form" action="">
-      <input type="hidden">
-      <p>
-        <label>
-          NAMN <br>
-          <input type="text" name="name">
-        </label>
-      </p>
-      <p>
-        <label>
-          E-POST<br>
-          <input type="text" name="email">
-        </label>
-      </p>
-      <p>
-        <label>
-          MEDDELANDE<br>
-          <textarea name="body" placeholder="Skriv ditt meddelande här..."></textarea>
-        </label>
-      </p>
-      <p>
-        <input class="footer__send-form-button" type="submit" value="Skicka">
-      </p>
-    </form>
-    <h4 class="footer__subheading">Kontakt</h4>
-    <p class="footer__info-heading">Mail</p>
-    <p class="footer__info-body">info@kallenfors.com</p>
-    <p class="footer__info-heading">Telefon</p>
-    <p class="footer__info-body">070-267 83 03</p>
-    <p class="footer__info-heading">Adress Stockholm</p>
-    <p class="footer__info-body">Bragevägen 21, <br> 112 24 Stockholm</p>
-    <p class="footer__info-heading">Adress Sundsvall</p>
-    <p class="footer__info-body">Skärsätt 118, <br> 862 95 Njurunda</p>
-    <h4 class="footer__subheading">Information</h4>
-    <p class="footer__info-heading">Fakturainfo</p>
-    <p class="footer__copyright">© 2021 KÄLLENFORS</p>
+    <div class="footer__wrapper">
+      <div class="footer__form-wrapper">
+        <h3 class="footer__heading">Maila oss</h3>
+        <form class="footer__form" action="">
+          <input type="hidden">
+          <p>
+            <label>
+              NAMN <br>
+              <input type="text" name="name">
+            </label>
+          </p>
+          <p>
+            <label>
+              E-POST<br>
+              <input type="text" name="email">
+            </label>
+          </p>
+          <p>
+            <label>
+              MEDDELANDE<br>
+              <textarea name="body" placeholder="Skriv ditt meddelande här..."></textarea>
+            </label>
+          </p>
+          <p>
+            <input class="footer__send-form-button" type="submit" value="Skicka">
+          </p>
+        </form>
+      </div>
+      <div class="footer__info-wrapper">
+        <h4 class="footer__subheading">Kontakt</h4>
+        <p class="footer__info-heading">Mail</p>
+        <p class="footer__info-body">info@kallenfors.com</p>
+        <p class="footer__info-heading">Telefon</p>
+        <p class="footer__info-body">070-267 83 03</p>
+        <p class="footer__info-heading">Adress Stockholm</p>
+        <p class="footer__info-body">Bragevägen 21, <br> 112 24 Stockholm</p>
+        <p class="footer__info-heading">Adress Sundsvall</p>
+        <p class="footer__info-body">Skärsätt 118, <br> 862 95 Njurunda</p>
+        <h4 class="footer__subheading">Information</h4>
+        <p class="footer__info-heading">Fakturainfo</p>
+        <p class="footer__copyright">© 2021 KÄLLENFORS</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -93,9 +99,15 @@ select:-webkit-autofill:focus
 .footer {
   background: $black;
   color: #fff;
-  display: flex;
-  flex-direction: column;
   padding: 20px;
+  margin: 0 auto;
+
+  &__wrapper {
+    max-width: 1200px;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
 
   &__heading {
     text-transform: uppercase;
@@ -143,6 +155,23 @@ select:-webkit-autofill:focus
   &__copyright {
     font-size: 12px;
     margin: 0;
+  }
+
+  @media only screen and (min-width: $tablet) {
+    
+    .footer {
+      &__wrapper {
+        flex-direction: row-reverse;
+      }
+
+      &__form-wrapper {
+        width: 50%;
+      }
+
+      &__info-wrapper {
+        width: 50%;
+      }
+    }
   }
 }
 </style>
