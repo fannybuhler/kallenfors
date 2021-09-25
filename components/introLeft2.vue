6b5c8de0226b21fb2d767b5fc96544a521010e9e
Fanny Buhlér <fanny.buhler@hyperisland.se>
2021-09-25 15:22:49 +0200

    edit nuxtlink

--- components/introLeft2.vue
+++ components/introLeft2.vue
@@ -6,7 +6,7 @@
         <img class="intro__image" src="@/assets/images/photos/peter.jpeg" alt="Porträttfoto av Peter Källenfors avslappnat klädd">
         <div class="intro__image-overlay">
           <h3 class="intro__image-overlay__heading">Peter<br>Källenfors</h3>
-          <NuxtLink to="/peter-kallenfors">
+          <NuxtLink class="intro__image-overlay__body" to="/peter-kallenfors">
             Läs mer om Peters arbete <i class="arrow right"></i>
           </NuxtLink>
         </div>
@@ -60,6 +60,20 @@ a {
     width: 100%;
     text-align: center;
     background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%);
+    padding: 10px 0;
+
+     &__heading {
+      color: $pop;
+      font-family: 'Playfair regular';
+      line-height: 130%;
+      font-size: 24px;
+      text-align: center;
+    }
+
+    &__body {
+      color: #fff;
+      font-size: 12px;
+    }
   }
 
   &__color-block {
@@ -76,75 +90,6 @@ a {
     }
   }
 
-  /*
-  margin: 3rem 0;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-
-  &__image-wrapper {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
-    position: relative;
-  }
-
-  &__color-block {
-    width: 85%;
-    background-color: #F8BD59;
-    z-index: -1;
-    grid-column: 1 / 4;
-    grid-row: 2 / 3;
-  }
-
-  &__image {
-    height: auto;
-    width: 70%;
-    margin-left: -12%;
-    z-index: 1;
-    max-height: 724px;
-    max-width: 550px;
-    grid-column: 1 / 4;
-    justify-self: center;
-    align-self: center;
-    grid-row: 1 / 4;
-  }
-
-  &__image-overlay {
-    background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%);
-    grid-column: 1 / 4;
-    grid-row: 1 / 4;
-    justify-self: center;
-    align-self: center;
-    height: 40%;
-    width: 70%;
-    max-height: 724px;
-    max-width: 550px;
-    position: absolute;
-    bottom: 0;
-    margin-left: -12%;
-    z-index: 2;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: center;
-
-    &__heading {
-      color: $pop;
-      font-family: 'Playfair regular';
-      line-height: 130%;
-      font-size: 24px;
-      text-align: center;
-      margin: 0;
-    }
-
-    &__body {
-      color: #fff;
-      font-size: 12px;
-    }
-  }
-  */
-
   &__content {
     flex: 1;
   }
@@ -160,20 +105,16 @@ a {
     height: 1px;
     width: 30%;
     margin: 20px 20px;
+    z-index: 2;
   }
 
   &__body {
     margin: 0 20px;
   }
 
-  /*
+  
   @media only screen and (min-width: $tablet) {
     .intro {
-      &__image-wrapper {
-        width: 50%;
-
-      }
-
       &__image-overlay {
         &__heading {
           font-size: 42px;
@@ -182,15 +123,6 @@ a {
         &__body {
           font-size: 18px;
         }
-      }
-
-      &__content-wrapper {
-        width: 50%;
-        align-self: center;
-      }
-
-      &__color-block {
-        width: 100%;
       }
 
       &__heading {
@@ -216,7 +148,5 @@ a {
       }
     }
   }
-  */
 }
-
 </style>
