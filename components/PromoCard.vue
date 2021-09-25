297759d8050cb3f1e51af8c56ea86d11c0e917f8
Björn <hermansson.bjorn@gmail.com>
2021-09-25 15:42:17 +0200

    Create promo card for promo cards component

---
+++ components/PromoCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="promo-card">
+    <img class="promo-card__icon" :src="imageUrl" />
+    <div class="promo-card__text">
+      <h3 class="promo-card__heading">{{ heading }}</h3>
+      <p class="promo-card__body">{{ leading }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: String,
+    leading: String,
+    image: String,
+    tags: Array
+  },
+
+  computed: {
+    imageUrl () {
+      if (this.$props.image.includes('https')) {
+        return this.$props.image
+      }
+
+      return require(`@/assets/images/${this.$props.image}`)
+    }
+  }
+}
+</script>
--- components/PromoCards.vue
+++ components/PromoCards.vue
@@ -2,7 +2,12 @@
   <div class="promo-cards">
     <h2 v-if="heading" class="promo-cards" v-text="heading" />
     <div v-for="item, index in items" :key="index">
-      item: {{ index }} {{ item.heading }}
+      <PromoCard
+        :heading="item.heading"
+        :leading="item.leading"
+        :image="item.image"
+        :tags="item.tags"
+      />
     </div>
   </div>
 </template>
